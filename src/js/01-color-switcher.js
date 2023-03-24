5b1d410cf055de81875bba1fd812fe531577c168
MariyaGanina <122280294+MariyaGanina@users.noreply.github.com>
2023-03-24 20:54:44 +0100

    hw9-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+let intervalId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startBackgroundChange() {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    document.querySelector('[data-start]').setAttribute('disabled', true);
+  }
+}
+
+function stopBackgroundChange() {
+  clearInterval(intervalId);
+  intervalId = null;
+  document.querySelector('[data-start]').removeAttribute('disabled');
+}
+
+document
+  .querySelector('[data-start]')
+  .addEventListener('click', startBackgroundChange);
+document
+  .querySelector('[data-stop]')
+  .addEventListener('click', stopBackgroundChange);
